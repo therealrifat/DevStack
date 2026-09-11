cb41ffbd462cbd2d22d32be6655114ab72c079ba
Rifat islam <merifatislam@gmail.com>
2026-09-11 16:02:19 +0600

    feat: add resposive design for mobile

--- src/component/Footer.tsx
+++ src/component/Footer.tsx
@@ -2,34 +2,34 @@ import footerImage from '../assets/logo-text.png'
 
 const Footer = () => {
     return (
-        <div className="max-w-11/12 mx-auto mt-50 space-y-3">
-           <div className='flex flex-row justify-between'>
-                <div className=' space-y-4'>
-                    <img src={footerImage} alt="" />
-                    <p className='text-[#64748B]'>Curated tools, technologies, and resources for developers building modern software.</p>
-                    <ul className='flex gap-3'>
+        <div className="w-191 md:w-full md:max-w-11/12 mx-auto mt-50 space-y-3">
+           <div className='flex flex-row justify-center  md:justify-between'>
+                <div className=' space-y-4  '>
+                    <img src={footerImage} alt="" className='mx-auto md:mx-0' />
+                    <p className='text-[#64748B] md:mx-0 mx-35 md:text-left text-center'>Curated tools, technologies, and resources for developers building modern software.</p>
+                    <ul className='flex gap-8 md:gap-3 justify-center md:justify-start'>
                         <li><a href="#">GitHub</a></li>
-                        <li><a href="#">Twitter</a></li>
-                        <li><a href="#">LinkedIn</a></li>
+                        <li className=' list-disc md:list-none'><a href="#">Twitter</a></li>
+                        <li className='list-disc md:list-none'><a href="#">LinkedIn</a></li>
                     </ul>
                 </div>
-                <div>
+                <div className=' hidden md:inline'>
                     <h4>PRODUCT </h4>
-                    <ul className='text-[#64748B]'>
+                    <ul className='  text-[#64748B]'>
                         <li><a href="#">Home </a></li>
                         <li><a href="#">Technologies </a></li>
                         <li><a href="#">Project </a></li>
                     </ul>
                 </div>
-                <div>
+                <div className=' hidden md:inline'>
                     <h4>COMPANY </h4>
-                    <ul className='text-[#64748B]'>
+                    <ul className=' text-[#64748B]'>
                         <li><a href="#">About </a></li>
                         <li><a href="#">Contact</a></li>
                         <li><a href="#">Careers </a></li>
                     </ul>
                 </div>
-                <div>
+                <div className='hidden md:inline'>
                     <h4>LEGAL</h4>
                     <ul className='text-[#64748B]'>
                          <li><a href="#">Privacy Policy </a></li>
@@ -37,7 +37,7 @@ const Footer = () => {
                     </ul>
                 </div>
            </div>
-           <div className='flex justify-between py-5 text-[#64748b]'>
+           <div className='flex justify-between py-5 px-5 md:px-0 text-[#64748b]'>
                 <p>© 2026 Dev Stack. All rights reserved.</p>
                 <div className='flex gap-5'>
                     <p>Privacy</p>
--- src/component/Navbar.tsx
+++ src/component/Navbar.tsx
@@ -14,7 +14,7 @@ const Navbar = () => {
                 <li><a href="#">Contact</a></li>
             </ul>
             <div className=' flex gap-4'>
-                <button className='text-[#475569] '>Sign in</button>
+                <button className='text-[#475569] '>Sign In</button>
                 <button className='bg-pink-500 text-white py-2 px-5 rounded-2xl '>Sign Up</button>
             </div>
         </nav>
