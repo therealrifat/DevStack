3ab08706e0dd0d79f17bb6ed464cb992be0c7886
Rifat islam <merifatislam@gmail.com>
2026-09-11 10:15:54 +0600

    feat: add resposive for mobile view

--- src/component/BannerSection.tsx
+++ src/component/BannerSection.tsx
@@ -3,16 +3,16 @@ import bannerPicture from '../assets/banner-stack.png'
 
 const BannerSection = () => {
     return (
-        <div className='flex justify-between items-center max-w-11/12 mx-auto space-y-2 p-5 gap-10 my-20'>
-            <div className='space-y-4 w-[50%] '>
-                <h2 className=" text-6xl font-extrabold">Build Your Idea <br/> <span className='bg-linear-to-r from-orange-800 to-blue-500 bg-clip-text text-transparent' >Development Stack</span></h2>
-                <h4 className='text-lg w-[75%] my-5 text-[#475569]'>Explore frontend, backend, database, and tooling options, compare them side by side, and put together the stack that fits your next project.</h4>
-                <div className='flex gap-5 my-18'>
-                    <button className='text-white font-medium py-2 px-5 rounded-lg bg-linear-to-r from-orange-600 to-pink-400'>Explore Technologies</button>
-                    <button className='border border-gray-300 py-2 px-6 rounded-lg'>Learn More</button>
+        <div className='w-191 md:w-full flex flex-col md:flex-row  justify-between items-center md:max-w-11/12 mx-auto space-y-2 p-5 gap-10 my-20'>
+            <div className='md:space-y-4 w-full md:w-[50%] text-center md:text-left '>
+                <h2 className="text-6xl md:font-extrabold font-bold">Build Your Ideal <br/> <span className='bg-linear-to-r from-orange-800 to-blue-500 bg-clip-text text-transparent' >Development Stack</span></h2>
+                <h4 className='md:text-lg text-2xl md:w-[75%] my-5 text-[#475569]'>Explore frontend, backend, database, and tooling options, compare them side by side, and put together the stack that fits your next project.</h4>
+                <div className='flex md:gap-5  gap-8 my-18 justify-center md:justify-start'>
+                    <button className='text-white font-medium md:text-[14px] md:py-2  md:px-5 text-2xl py-5 px-10 rounded-lg bg-linear-to-r from-orange-600 to-pink-400'>Explore Technologies</button>
+                    <button className='border border-gray-300 md:text-[14px] md:py-2 md:px-6 text-2xl py-5 px-18  rounded-lg'>Learn More</button>
                 </div>
             </div>
-            <img src={bannerPicture} alt="" className=' w-120' />
+            <img src={bannerPicture} alt="" className='md:-mt-10 -mt-55 md:w-150 w-190' />
         </div>
 
     );
--- src/component/Navbar.tsx
+++ src/component/Navbar.tsx
@@ -1,10 +1,12 @@
 import navLogo from '../assets/logo-text.png'
+import menuIcon from '../assets/hamburger.png'
 
 const Navbar = () => {
     return (
-        <nav className='flex justify-between p-5 items-center max-w-11/12 mx-auto'>
-            <img src={navLogo} alt=""  className='w-40' />
-            <ul className=' flex gap-7 text-md text-[#475569]'>
+        <nav className='w-191  mx-2 md:w-full flex justify-between p-5 items-center md:max-w-11/12 md:mx-auto '>
+            <img src={menuIcon} alt=""  className=' w-10 md:hidden'/>
+            <img src={navLogo} alt=""  className='w-40 md:w-40' />
+            <ul className=' hidden md:flex gap-7 text-md text-[#475569]'>
                 <li><a href="#">Home </a></li>
                 <li><a href="#">Technologies</a></li>
                 <li><a href="#">Projects</a></li>
@@ -12,7 +14,7 @@ const Navbar = () => {
                 <li><a href="#">Contact</a></li>
             </ul>
             <div className=' flex gap-4'>
-                <button className='text-[#475569] font-'>Sign in</button>
+                <button className='text-[#475569] '>Sign in</button>
                 <button className='bg-pink-500 text-white py-2 px-5 rounded-2xl '>Sign Up</button>
             </div>
         </nav>
